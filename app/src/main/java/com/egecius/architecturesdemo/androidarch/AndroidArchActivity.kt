7ab2ac501cbf493caf1ddd0f6d3c56fd0a778d14
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 17:12:44 +0100

    implement most of AndroidArchActivity.kt

--- app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
@@ -1,16 +1,89 @@
 package com.egecius.architecturesdemo.androidarch
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.egecius.architecturesdemo.R
+import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarClick
+import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarDetailActivity
+import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarRecyclerViewAdapter
+import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.OnCarClickListener
+import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCar
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_android_arch.*
 
 class AndroidArchActivity : AppCompatActivity() {
 
-    private val model: AndroidArchViewModel by viewModels()
+    private val viewModel: AndroidArchViewModel by viewModels()
+
+    private val carRecyclerViewAdapter = CarRecyclerViewAdapter(object : OnCarClickListener {
+        override fun onClick(carClick: CarClick) {
+            CarDetailActivity.start(this@AndroidArchActivity, carClick)
+        }
+    })
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_android_arch)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        showCarsWhenAvailable()
+        setupRecycler()
+        updateProgressBar()
+    }
+
+    private fun updateProgressBar() {
+        viewModel.isUpdating.observe(this, Observer { isUpdating ->
+            if (isUpdating) {
+                showLoadingInProgress()
+            } else {
+                showRecyclerViewOnly()
+            }
+        })
+        viewModel.isError.observe(this, Observer { isError ->
+            if (isError) {
+                showLoadingError()
+            }
+        })
+    }
+
+    private fun showCarsWhenAvailable() {
+        viewModel.carsList.observe(this, Observer {
+            showCars(it)
+        })
+    }
+
+    private fun showCars(cars: List<UiCar>) {
+        carRecyclerViewAdapter.setData(cars)
+    }
+
+    private fun showRecyclerViewOnly() {
+        progress_bar.visibility = View.GONE
+        recycler_view.visibility = View.VISIBLE
+    }
+
+    private fun setupRecycler() {
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = carRecyclerViewAdapter
+    }
+
+    private fun showLoadingError() {
+        Snackbar.make(parent_layout, getString(R.string.loading_error), Snackbar.LENGTH_INDEFINITE)
+            .setAction(getString(R.string.retry)) {
+                viewModel.retryFetching()
+            }
+            .show()
+        showRecyclerViewOnly()
+    }
+
+    private fun showLoadingInProgress() {
+        progress_bar.visibility = View.VISIBLE
+        recycler_view.visibility = View.GONE
     }
 }
