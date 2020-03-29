29bd19aa47d59149e9d1ad64b080e67f8759d7e3
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 13:44:19 +0100

    add Retrofit

---
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/a_frameworks/retrofit/MockWebSeverInitializer.kt
@@ -0,0 +1,30 @@
+package com.egecius.architecturesdemo.cleanarch.a_frameworks.retrofit
+
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import okhttp3.mockwebserver.MockWebServer
+
+class MockWebSeverInitializer {
+
+    private lateinit var mockWebServer: MockWebServer
+
+    fun init() {
+        setupMockWebSever()
+    }
+
+    private fun setupMockWebSever() {
+        mockWebServer = MockWebServer()
+        mockWebServer.setDispatcher(MockWebServerDispatcher())
+
+        Completable.fromCallable { mockWebServer.start(PORT) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    companion object {
+        const val PORT = 54034
+        const val BASE_URL: String = "http://localhost:$PORT"
+    }
+}
---
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/a_frameworks/retrofit/RetrofitAdapter.kt
@@ -0,0 +1,48 @@
+package com.egecius.architecturesdemo.cleanarch.a_frameworks.retrofit
+
+import com.egecius.architecturesdemo.cleanarch.d_domain.Car
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class RetrofitAdapter {
+
+    private val mockWebServerBaseUrl = MockWebSeverInitializer.BASE_URL
+    private val baseUrlHeroku = "https://mighty-spire-24044.herokuapp.com/"
+
+    fun setupRetrofit(): CarRetrofitService {
+        return Retrofit.Builder()
+            .baseUrl(mockWebServerBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createLoggingOkHttpClient())
+            .build()
+            .create(CarRetrofitService::class.java)
+    }
+
+    private fun createLoggingOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+}
+
+interface CarRetrofitService {
+
+    // only works with Heroku base url
+    @GET("electric")
+    fun getCarsByPages(@Query("page") page: Int): Call<List<Car>>
+
+    @GET(ENDPOINT_CARS_FULL)
+    suspend fun getCarsFull(): List<Car>
+
+    companion object {
+        const val ENDPOINT_CARS_FULL = "electric_full"
+    }
+
+}
