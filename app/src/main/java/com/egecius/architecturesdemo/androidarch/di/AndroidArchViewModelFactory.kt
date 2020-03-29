f0570ce17622fd8275a466d40f31f000d597f678
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 17:21:58 +0100

    add AndroidArchViewModelFactory.kt

---
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/di/AndroidArchViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.egecius.architecturesdemo.androidarch.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.egecius.architecturesdemo.androidarch.AndroidArchViewModel
+import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCarsMapper
+import com.egecius.architecturesdemo.cleanarch.d_domain.CarsRepo
+
+class AndroidArchViewModelFactory(
+    private val carsRepository: CarsRepo,
+    private val uiCarsMapper: UiCarsMapper
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST") // taken from Google sample
+        return AndroidArchViewModel(carsRepository, uiCarsMapper) as T
+    }
+}
