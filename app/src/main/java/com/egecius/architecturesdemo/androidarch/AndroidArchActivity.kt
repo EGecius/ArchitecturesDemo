6119421ee1c8166d05e9a59cb1a2fb88e0907a99
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 17:24:57 +0100

    inject into AndroidArchActivity.kt

--- app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
@@ -7,11 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.egecius.architecturesdemo.R
+import com.egecius.architecturesdemo.androidarch.di.AndroidArchActivityModule
+import com.egecius.architecturesdemo.androidarch.di.DaggerAndroidArchActivityComponent
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarClick
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarDetailActivity
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarRecyclerViewAdapter
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.OnCarClickListener
 import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCar
+import com.egecius.architecturesdemo.cleanarch.di.DaggerCleanArchMainActivityComponent
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_android_arch.*
 
@@ -29,7 +32,14 @@ class AndroidArchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_android_arch)
+        injectDependencies()
         setupUi()
+    }
+
+    private fun injectDependencies() {
+        DaggerAndroidArchActivityComponent.builder()
+            .androidArchActivityModule(AndroidArchActivityModule(this))
+            .build().injectInto(this)
     }
 
     private fun setupUi() {
