fa2d8c3256c241ad10a8d518adf236298f9cf470
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 17:34:24 +0100

    add subcomponents to AppComponent

--- app/src/main/java/com/egecius/architecturesdemo/cleanarch/shared/di/AppComponent.kt
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/shared/di/AppComponent.kt
@@ -1,5 +1,9 @@
 package com.egecius.architecturesdemo.cleanarch.shared.di
 
+import com.egecius.architecturesdemo.androidarch.di.AndroidArchActivityComponent
+import com.egecius.architecturesdemo.androidarch.di.AndroidArchActivityModule
+import com.egecius.architecturesdemo.cleanarch.di.CleanArchMainActivityComponent
+import com.egecius.architecturesdemo.cleanarch.di.CleanArchMainActivityModule
 import com.egecius.architecturesdemo.cleanarch.shared.MyApplication
 import dagger.Component
 import javax.inject.Singleton
@@ -14,5 +18,7 @@ interface AppComponent {
 
     fun injectInto(psApplication: MyApplication)
 
-//    fun plus(homeActivityModule: HomeActivityModule): HomeActivityComponent
+    fun plus(module: AndroidArchActivityModule): AndroidArchActivityComponent
+
+    fun plus(module: CleanArchMainActivityModule): CleanArchMainActivityComponent
 }
