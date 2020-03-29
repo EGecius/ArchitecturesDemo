3ee3be8230d2f68ca49324025eba3db3f938e077
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 13:45:45 +0100

    add Room

---
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/a_frameworks/room/CarsDatabase.kt
@@ -0,0 +1,35 @@
+package com.egecius.architecturesdemo.cleanarch.a_frameworks.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.egecius.architecturesdemo.cleanarch.b_adapters.CarDao
+import com.egecius.architecturesdemo.cleanarch.d_domain.Car
+
+@Database(entities = [Car::class], version = 1, exportSchema = false)
+abstract class CarsDatabase : RoomDatabase() {
+
+    abstract fun carDao(): CarDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: CarsDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): CarsDatabase {
+
+            val snapshot = INSTANCE
+
+            return if (snapshot != null) {
+                snapshot
+            } else {
+                val newInstance = Room.databaseBuilder(context, CarsDatabase::class.java, "Sample.db").build()
+                INSTANCE = newInstance
+                newInstance
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/b_adapters/CarDao.kt
@@ -0,0 +1,21 @@
+package com.egecius.architecturesdemo.cleanarch.b_adapters
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.egecius.architecturesdemo.cleanarch.d_domain.Car
+
+@Dao
+interface CarDao {
+
+    @Query("SELECT * FROM cars")
+    suspend fun loadAllCars(): List<Car>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCar(car: Car)
+
+    @Query("DELETE FROM Cars")
+    fun deleteAllCars()
+
+}
