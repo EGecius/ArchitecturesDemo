228321744b6e829d6a2fc7e3fd3c0df563c8514c
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 15:25:06 +0100

    allow clear traffic for mock webserver

--- app/src/main/java/com/egecius/architecturesdemo/cleanarch/a_frameworks/retrofit/MockWebServerDispatcher.kt
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/a_frameworks/retrofit/MockWebServerDispatcher.kt
@@ -1,5 +1,6 @@
 package com.egecius.architecturesdemo.cleanarch.a_frameworks.retrofit
 
+import android.util.Log
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
@@ -8,12 +9,15 @@ import java.util.*
 class MockWebServerDispatcher : Dispatcher() {
 
     override fun dispatch(request: RecordedRequest?): MockResponse {
+    	Log.v("Eg:MockWebServerDispatcher:12", "dispatch()")
 
         Thread.sleep(NETWORK_DELAY_MILLIS)
 
         if (request?.path.equals("/" + NetworkService.ENDPOINT_CARS_FULL)) {
+            Log.v("Eg:MockWebServerDispatcher:15", "dispatch() ")
             return MockResponse().setBody(getElectricCars())
         }
+        Log.v("Eg:MockWebServerDispatcher:19", "dispatch() 404")
         return MockResponse().setResponseCode(404)
     }
 
