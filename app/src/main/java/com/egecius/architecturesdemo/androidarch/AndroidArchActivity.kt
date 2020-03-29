5fb9a9f8178c49bd3c0c8d4e94073a8f8726d99d
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 17:02:03 +0100

    add AndroidArchActivity

---
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
@@ -0,0 +1,13 @@
+package com.egecius.architecturesdemo.androidarch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.egecius.architecturesdemo.R
+
+class AndroidArchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_arch)
+    }
+}
