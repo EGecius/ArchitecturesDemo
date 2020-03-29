70a2d67b3371d74cbc492c5524e2dc175be7527b
Egidijus Gecius <e.gecius@gmail.com>
2020-03-29 16:26:01 +0100

    add NetworkCarMapper.kt

--- app/src/main/java/com/egecius/architecturesdemo/cleanarch/b_adapters/network/NetworkCarMapper.kt
+++ app/src/main/java/com/egecius/architecturesdemo/cleanarch/b_adapters/network/NetworkCarMapper.kt
@@ -1,3 +1,14 @@
 package com.egecius.architecturesdemo.cleanarch.b_adapters.network
 
-class NetworkCarMapper
+import com.egecius.architecturesdemo.cleanarch.d_domain.Car
+
+class NetworkCarMapper {
+
+    fun toCars(jsonList: List<JsonCar>): List<Car> {
+        return mutableListOf<Car>().apply {
+            jsonList.forEach {
+                add(Car(it.name, it.img))
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/egecius/architecturesdemo/cleanarch/b_adapters/network/NetworkCarMapperTest.kt
@@ -0,0 +1,34 @@
+package com.egecius.architecturesdemo.cleanarch.b_adapters.network
+
+import com.egecius.architecturesdemo.cleanarch.d_domain.Car
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class NetworkCarMapperTest {
+
+    private lateinit var sut: NetworkCarMapper
+
+    @Before
+    fun setUp() {
+        sut = NetworkCarMapper()
+    }
+
+    @Test
+    fun `maps to UI cars`() {
+        val carsList = listOf(
+            JsonCar("Tesla Y", "img 1"),
+            JsonCar("VW e-golf", "img 2")
+        )
+
+        val resultList = sut.toCars(carsList)
+
+        assertThat(resultList.size).isEqualTo(2)
+        assertThat(resultList[0]).isEqualTo(Car("Tesla Y", "img 1"))
+        assertThat(resultList[1]).isEqualTo(Car("VW e-golf", "img 2"))
+    }
+
+}
