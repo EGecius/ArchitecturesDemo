db2bb2ac9e215a9d365cdf196ca6ad068808714e
Egidijus Gecius <e.gecius@gmail.com>
2020-04-18 14:06:31 +0100

    re-fetch data on 'retry' clicked

--- app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModel.kt
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModel.kt
@@ -1,5 +1,6 @@
 package com.egecius.architecturesdemo.androidarch
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
@@ -14,20 +15,24 @@ class AndroidArchViewModel constructor(
     private val carsRepository: CarsRepo,
     private val uiCarsMapper: UiCarsMapper,
     private val navigator: Navigator,
-    dispatcher: CoroutineDispatcher
+    private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     // TODO: 18/04/2020 remove it, once tests for  AndroidArchViewModel are finished
     val liveDataDemo = MutableLiveData(1)
     val isError = MutableLiveData(false)
 
-    val carsList = liveData<List<UiCar>>(dispatcher) {
-        try {
-            val cars = carsRepository.getCars()
-            uiCarsMapper.toUiCars(cars)
-            isError.value = false
-        } catch (e: Exception) {
-            isError.value = true
+    var carsList = fetchCars(dispatcher)
+
+    private fun fetchCars(dispatcher: CoroutineDispatcher): LiveData<List<UiCar>> {
+        return liveData(dispatcher) {
+            try {
+                val cars = carsRepository.getCars()
+                uiCarsMapper.toUiCars(cars)
+                isError.value = false
+            } catch (e: Exception) {
+                isError.value = true
+            }
         }
     }
 
@@ -36,7 +41,7 @@ class AndroidArchViewModel constructor(
     }
 
     fun onClickedRetry() {
-        TODO("not implemented")
+        isError.value = false
+        carsList = fetchCars(dispatcher)
     }
-
 }
