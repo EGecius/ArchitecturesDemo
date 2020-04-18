8555ff8ad15accc51bf774680c7e5a5108a2b391
Egidijus Gecius <e.gecius@gmail.com>
2020-04-18 11:27:11 +0100

    inject AndroidArchViewModel

--- app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchActivity.kt
@@ -1,13 +1,18 @@
 package com.egecius.architecturesdemo.androidarch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.egecius.architecturesdemo.androidarch.di.AndroidArchActivityModule
 import com.egecius.architecturesdemo.cleanarch.di.CleanArcActivityModule
 import com.egecius.architecturesdemo.cleanarch.shared.MyApplication
 import com.egecius.architecturesdemo.databinding.ActivityAndroidArchBinding
+import javax.inject.Inject
 
 class AndroidArchActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var androidViewModel: AndroidArchViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
