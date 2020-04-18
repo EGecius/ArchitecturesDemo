e91b0501ecdc585812db9c62e345199217a50b67
Egidijus Gecius <e.gecius@gmail.com>
2020-04-18 14:25:48 +0100

    use viewModelScope

--- app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModel.kt
+++ app/src/main/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModel.kt
@@ -1,15 +1,13 @@
 package com.egecius.architecturesdemo.androidarch
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.*
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.CarClick
 import com.egecius.architecturesdemo.cleanarch.a_frameworks.android.Navigator
 import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCar
 import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCarsMapper
 import com.egecius.architecturesdemo.cleanarch.d_domain.CarsRepo
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 
 class AndroidArchViewModel constructor(
     private val carsRepository: CarsRepo,
@@ -21,18 +19,18 @@ class AndroidArchViewModel constructor(
     // TODO: 18/04/2020 remove it, once tests for  AndroidArchViewModel are finished
     val liveDataDemo = MutableLiveData(1)
     val isError = MutableLiveData(false)
+    var carsList = MutableLiveData<List<UiCar>>()
 
-    var carsList = fetchCars(dispatcher)
+    init {
+        fetchCarsList()
+    }
 
-    private fun fetchCars(dispatcher: CoroutineDispatcher): LiveData<List<UiCar>> {
-        return liveData(dispatcher) {
-            try {
-                val cars = carsRepository.getCars()
-                uiCarsMapper.toUiCars(cars)
-                isError.value = false
-            } catch (e: Exception) {
-                isError.value = true
-            }
+    private fun fetchCarsList() {
+        viewModelScope.launch {
+            val cars = carsRepository.getCars()
+            carsList.value = uiCarsMapper.toUiCars(cars)
+        }.invokeOnCompletion {
+            isError.value = true
         }
     }
 
@@ -42,6 +40,6 @@ class AndroidArchViewModel constructor(
 
     fun onClickedRetry() {
         isError.value = false
-        carsList = fetchCars(dispatcher)
+        fetchCarsList()
     }
 }
