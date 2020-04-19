ff20a062824b5f85b0332e58f61178b03917a741
Egidijus Gecius <e.gecius@gmail.com>
2020-04-19 08:31:20 +0100

    show error message

--- app/src/test/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModelTest.kt
+++ app/src/test/java/com/egecius/architecturesdemo/androidarch/AndroidArchViewModelTest.kt
@@ -10,6 +10,7 @@ import com.egecius.architecturesdemo.cleanarch.b_adapters.ui.UiCarsMapper
 import com.egecius.architecturesdemo.cleanarch.d_domain.Car
 import com.egecius.architecturesdemo.cleanarch.d_domain.CarsRepo
 import com.egecius.architecturesdemo.utils.MainCoroutineRule
+import com.nhaarman.mockitokotlin2.doThrow
 import com.nhaarman.mockitokotlin2.given
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
@@ -17,7 +18,6 @@ import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,19 +33,22 @@ class AndroidArchViewModelTest {
 
     @Mock
     private lateinit var carsRepository: CarsRepo
+
     @Mock
     private lateinit var navigator: Navigator
+
     @Mock
     private lateinit var uiCarsMapper: UiCarsMapper
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
     /** Sets the main coroutines dispatcher to a TestCoroutineScope for unit testing */
     @ExperimentalCoroutinesApi
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+    private val testDispatcher = TestCoroutineDispatcher()
 
     private val carsList = listOf(Car("name_1", "img_2"))
     private val uiCarsList = listOf(UiCar("name_1", "img_2"))
@@ -54,11 +57,11 @@ class AndroidArchViewModelTest {
     @Before
     fun setUp() {
         given(uiCarsMapper.toUiCars(carsList)).willReturn(uiCarsList)
-        sut = AndroidArchViewModel(carsRepository, uiCarsMapper, navigator, testCoroutineDispatcher)
+        sut = AndroidArchViewModel(carsRepository, uiCarsMapper, navigator, testDispatcher)
     }
 
     @Test
-    fun `shows cars List`() = testCoroutineDispatcher.runBlockingTest {
+    fun `shows cars List`() = testDispatcher.runBlockingTest {
         givenCarsWillBeEmitted()
 
         sut.onCreate()
@@ -71,15 +74,18 @@ class AndroidArchViewModelTest {
         given(carsRepository.getCars()).willReturn(carsList)
     }
 
-    @Test @Ignore
-    fun `shows error message`() {
+    @Test
+    fun `shows error message`() = testDispatcher.runBlockingTest {
         givenCarsEmissionFails()
 
-        // TODO: 18/04/2020 finish
+        sut.onCreate()
+
+        val result: Boolean? = sut.isError.value
+        assertThat(result).isTrue()
     }
 
-    private fun givenCarsEmissionFails() {
-        TODO("not implemented")
+    private suspend fun givenCarsEmissionFails() {
+        doThrow(RuntimeException::class).`when`(carsRepository).getCars()
     }
 
     @Test
@@ -93,7 +99,7 @@ class AndroidArchViewModelTest {
     fun `viewModelScope job intercepts exception in invokeOnCompletion`() {
         var resultThrowable: Throwable? = null
 
-        sut.viewModelScope.launch{
+        sut.viewModelScope.launch {
             throw Exception("egis")
         }.invokeOnCompletion {
             resultThrowable = it
